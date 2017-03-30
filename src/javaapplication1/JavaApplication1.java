8fc9867beb7cf67833b55e4e2a63b894a68b64d7
7659186 <7659186@P3220410-171222.fmu.interno>
2017-03-30 19:33:10 -0300

    Fazendo IF testando a variavel C com a palavra 'teste'

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -15,7 +15,15 @@ public class JavaApplication1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        String c="teste";
+        
         System.out.println("Teste");
+        if (c.equals("teste")){
+            System.out.println("Ae krai.. mata onça mesmo aqui tem coragi");
+            }
+        else {
+            System.out.println("nao");
+        }
     }
     
 }
